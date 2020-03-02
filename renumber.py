1f019fd9681c86567a3e51bf391c8d13ba098a60
palbarede <pa.news@free.fr>
2020-03-02 18:03:54 +0000

    simplified the while loop

--- renumber.py
+++ renumber.py
@@ -3,17 +3,18 @@ def renumber(list1):
     list2=[]+list1
     i=0
     downshift=list2[0]
-    while i<len(list2)-1:
+    while True:
         list2[i]-=downshift
+        if i>=len(list2)-1: # if last element break out
+            break
         if list2[i+1]-downshift>list2[i]+1:
             downshift += list2[i+1]-downshift-(list2[i]+1)
         i+=1
-    list2[-1]-=downshift
     return list2
 
-# # test
-# list1=[3,3,5,8,10,10,13,15,15]
-# list1=[-3,-3,-3,5,5,5,7,7,9]
-# list2=renumber(list1)
-# for i in range(len(list1)):
-#     print(list1[i],list2[i])
+# test
+list1=[3,3,5,8,10,10,13,15,15]
+#list1=[-3,-3,-3,5,5,5,7,7,9]
+list2=renumber(list1)
+for i in range(len(list1)):
+    print(list1[i],list2[i])
